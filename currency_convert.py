26bc312b9ce277581fdace8dfe745dd1eaa87433
Lee Eakes <leeeakes@gmail.com>
2015-01-20 16:37:15 -0500

    dollar to euro

--- currency_convert.py
+++ currency_convert.py
@@ -1,38 +1,8 @@
 #RATES list should be a list of tuples, with each tuple containing a currency
 #code you can convert from, a currency code you can convert to, and a rate.
 
-rates = [("USD", "EUR", .74)]
-
 def convert(rates, value, orig_cur, new_cur):
     if orig_cur == new_cur:
         return value
-    conversion = round(value * rates, 2)
+    conversion = conversion = round(value * rates[0][2], 2)
     return conversion
-
-#def get_rate():
-
-
-#Create a function called convert that takes
-#a list called rates,
-#a number called value,
-#a string called from,
-#and a string called to.
-
-#rates = [("USD", "EUR", .74)]
-#This means that each dollar is worth 0.74 euros.
-
-#value is the amount of currency,
-#from is the current currency code
-#and to is the currency code you wish to convert to.
-
-#Given the above rates, make sure that converting 1 dollar into euros
-#returns the following value: 0.74.
-
-#Make sure than when you call convert with from and to being equal,
-#the return value is the same as value.
-
-
-#USD to EUR,
-#EUR to USD
-#EUR to JPY
-#JPY to EUR.
--- test_currency.py
+++ test_currency.py
@@ -1,16 +1,9 @@
 from currency_convert import *
 
-rates = [("USD", "EUR", 1.25)]
+rates = [("USD", "EUR", .74)]
 
 def test_convert_match():
-    assert convert(2, 1, "COC", "COC") == 1
+    assert convert(rates, 1, "USD", "USD") == 1
 
 def test_convert_rate():
-    assert convert(2, 1, "USD", "COC") == 2
-    assert convert(.75, 1.5, "USD", "COC") == 1.12
-
-#def convert(rates, value, orig_cur, new_cur):
-#    if orig_cur == new_cur:
-#        return value
-#    conversion = value * rates
-#    return conversion
+    assert convert(rates, 1, "USD", "EUR") == .74
