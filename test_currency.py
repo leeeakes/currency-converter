19cafbdf13ff4a350633feee13aba2ea3e8f3f1a
Lee Eakes <leeeakes@gmail.com>
2015-01-20 15:01:52 -0500

    Test if equal

---
+++ test_currency.py
@@ -0,0 +1,13 @@
+from currency_convert import *
+
+rates = [("USD", "EUR", 1.25)]
+
+def test_convert_match():
+    assert convert(2, 1, "COS", "COS") == 1
+
+
+
+
+#rates = [("USD", "EUR", .74)]
+
+#def convert(rates, value, orig_cur, new_cur):
